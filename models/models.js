64f021411ff7d518b65ae22870ba317a2dc5f479
earnedmarketing <tomkisss@gmail.com>
2015-07-05 20:19:56 +0100

    Heroku Postgres DB

--- models/models.js
+++ models/models.js
@@ -1,11 +1,29 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgress://user:passwd@host:port/database
+// SQlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match( /(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/ );
+var DB_name     = ( url[6] || null );
+var user        = ( url[2] || null );
+var pwd         = ( url[3] || null );
+var protocol    = ( url[1] || null );
+var dialect     = ( url[1] || null );
+var port        = ( url[5] || null );
+var host        = ( url[4] || null );
+var storage = process.env.DATABASE_STORAGE;
+
 // cargar modelo ORM
 var Sequelize = require('sequelize');
 
-// user BD SQLite
-var sequelize = new Sequelize( null, null, null,
-        { dialect: 'sqlite', storage: 'quiz.sqlite' }
+// usar BD SQLite
+var sequelize = new Sequelize( DB_name, user, pwd,
+        {   dialect : protocol,
+            protocol: protocol,
+            port    : port,
+            host    : host,
+            storage : storage, // solo SQLite (.env)
+            omitNull : true // solo Postgres
+        }
 );
 
 // importar la definicion de la table Quiz en quiz.js
