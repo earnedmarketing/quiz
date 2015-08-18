c24eea39d750f8442c51414be4ab0a593167691d
earnedmarketing <tomkisss@gmail.com>
2015-08-18 23:54:14 +0100

    Comment moderation

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,11 @@ module.exports = function( sequelize, DataTypes ) {
     return sequelize.define( 'Comment',
         {   texto: {
                 type: DataTypes.STRING,
-                validate: { notEmpty: { msg: "-> Falta Tema" } }
+                validate: { notEmpty: { msg: "-> Falta comentario" } }
+            },
+            publicado: {
+                    type: DataTypes.BOOLEAN,
+                    defaultValue: false
             }
         }
     );
--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,9 @@ router.get( '/author', function( req, res ) {
     res.render( 'author', { errors: [] } );
 });
 
-// GET Autoload de comandos con :quizId
-router.param( 'quizId', quizController.load );
+// Autoload de comandos con ID en el URL
+router.param( 'quizId',     quizController.load );
+router.param( 'commentId',  commentController.load );
 
 // definicion de rutas de session
 router.get(     '/login',       sessionController.new );
@@ -33,7 +34,8 @@ router.get(     '/quizes/:quizId(\\d+)/edit',      sessionController.loginRequir
 router.put(     '/quizes/:quizId(\\d+)',           sessionController.loginRequired, quizController.update );
 router.delete(  '/quizes/:quizId(\\d+)',           sessionController.loginRequired, quizController.destroy );
 
-router.get(     '/quizes/:quizId(\\d+)/comments/new',       commentController.new );
-router.post(    '/quizes/:quizId(\\d+)/comments',           commentController.create );
+router.get(     '/quizes/:quizId(\\d+)/comments/new',                       commentController.new );
+router.post(    '/quizes/:quizId(\\d+)/comments',                           commentController.create );
+router.get(     '/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',  sessionController.loginRequired, commentController.publish );
 
 module.exports = router;
